bb810d63355e2d90055d6cf5a94927f4fcef9a88
Viliam Páleník <vil.palenik@gmail.com>
2025-03-17 20:54:48 +0100

    komentar

--- zad_1/src/Main.java
+++ zad_1/src/Main.java
@@ -1,4 +1,13 @@
 
+
+/** Vypracoval: Viliam Páleník
+ * Java verzia:
+    java version "19.0.2" 2023-01-17
+    Java(TM) SE Runtime Environment (build 19.0.2+7-44)
+    Java HotSpot(TM) 64-Bit Server VM (build 19.0.2+7-44, mixed mode, sharing)
+ * Vývojové prostredie:
+    Visual Studio Code  
+ */
 public class Main {
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 3, 4, 5};
